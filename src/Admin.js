3e74cf0e49e9de8c5cef35d44e69dec778cb0afe
nasserrgithub <34746890+nasserrgithub@users.noreply.github.com>
2020-09-18 17:03:00 +0800

    Update Admin.js

--- src/Admin.js
+++ src/Admin.js
@@ -23,7 +23,7 @@ class Admin extends Component {
 
 
   	onLogin = () => {
-  		fetch(`${process.env.REACT_APP_URL}/admin`, {
+  		fetch(`${process.env.DOM}/admin`, {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
